029346d8d75368e7f7d5fc705b9eba6843a58ba6
Shrey42-dot <shrey42pandey@gmail.com>
2025-11-18 23:57:56 +0530

    Serve secret via frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
-import CreateSecret from "./pages/CreateSecret.jsx";
-import ViewSecret from "./pages/ViewSecret.jsx";
-import "./index.css";
+import App from "./App";
+import CreateSecret from "./pages/CreateSecret";
+import ViewSecret from "./pages/ViewSecret";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<CreateSecret />} />
           <Route path="s/:token" element={<ViewSecret />} />
+          {/* other routes */}
         </Route>
       </Routes>
     </BrowserRouter>
